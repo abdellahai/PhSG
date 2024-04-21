e4391c6a4b1de829174b48709ce90c6e99fbb4d5
Abdellah IDRISSI AZAMI <idrissi.azami.abdellah@gmail.com>
2024-04-21 20:16:44 +0100

    Update home.jsx

--- appt/src/pages/home.jsx
+++ appt/src/pages/home.jsx
@@ -3,7 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../widgets/navbar";
 
 function Home() {
-  const items = ["Temara", "Casablanca", "Rabat", "Nador", "Fes"];
+  let posts = [
+    { title: "P1", content: "lorem ipsum" },
+    { title: "P2", content: "lorem ipsum" },
+    { title: "P3", content: "lorem ipsum" },
+    { title: "P4", content: "lorem ipsum" },
+    { title: "P5", content: "lorem ipsum" },
+  ];
 
   return (
     <>
@@ -11,12 +17,14 @@ function Home() {
         <NavBar />
       </header>
       <h1>Posts</h1>
+      {posts.length === 0 ? <p>No post found</p> : null}
       <ul className="list-group">
-        {items.map(
-          (item) => (
-            <li className="list-group-item">{item}</li>
-          ) //call-back function
-        )}
+        {posts.map((post, index) => (
+          <li className="list-group-item" key={index}>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+          </li>
+        ))}
       </ul>
     </>
   );
